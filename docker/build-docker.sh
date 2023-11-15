e241c8ae52e07668dc958f1945009278f8001bfa
Fernando Dumont <fernando@fernando.com.br>
2023-11-15 11:25:59 -0300

    Containers

---
+++ docker/build-docker.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+CONTAINER_VERSION=1.0
+DOCKER_DOMAIN=fernandodumont
+
+BUILD_MA=false
+BUILD_OTEL=false
+
+for ARGUMENT in "$@"
+do
+    if [ "$ARGUMENT" = "otel" ]; then
+        BUILD_OTEL=true
+    elif [ "$ARGUMENT" = "ma" ]; then
+        BUILD_MA=true
+    elif [ "$ARGUMENT" = "all" ]; then
+        BUILD_OTEL=true
+        BUILD_MA=true
+    fi
+done
+
+export DOCKER_BUILDKIT=1;
+
+if [ $BUILD_OTEL == true ];then
+    echo ""
+    echo "==> OTEL"
+    ( 
+        docker buildx build --pull -t ${DOCKER_DOMAIN}/otelcol-appdynamics:${CONTAINER_VERSION} -o type=docker --platform=linux/arm64 -f otelcol/Dockerfile ..
+    )
+fi
+
+if [ $BUILD_MA == true ];then
+    echo ""
+    echo "==> Machine Agent"
+    ( 
+        docker buildx build --pull -t ${DOCKER_DOMAIN}/machine-agent:${CONTAINER_VERSION} -o type=docker --platform=linux/arm64 -f machine-agent/Dockerfile .
+    )
+fi
+
